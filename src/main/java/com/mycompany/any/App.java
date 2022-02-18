e76cff9b4c263cd48f13f11db5ae78b4953efba5
lumibar <47082617+lumibar@users.noreply.github.com>
2022-02-18 15:06:46 +0000

    testy

--- src/main/java/com/mycompany/any/App.java
+++ src/main/java/com/mycompany/any/App.java
@@ -1,13 +1,24 @@
 package com.mycompany.any;
 
+import ch.bildspur.artnet.ArtNetClient;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        byte[] dmxData = new byte[512];
+        ArtNetClient artnet = new ArtNetClient();
+        artnet.start();
+
+        // set data
+        for (int i = 0; i < dmxData.length; i++) {
+            dmxData[i] = (byte) 255;
+        }
+
+        // send data to localhost
+        artnet.unicastDmx("172.16.0.1", 0, 0, dmxData);
+
     }
 }
