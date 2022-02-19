d0c061e577c5a6264a1b24b1fd3047c7d46497b6
lumibar <47082617+lumibar@users.noreply.github.com>
2022-02-19 18:40:39 +0000

    more fixes

--- src/main/java/com/mycompany/any/FixturePackage/AmericanDJMegaBar.java
+++ src/main/java/com/mycompany/any/FixturePackage/AmericanDJMegaBar.java
@@ -5,11 +5,13 @@ import com.mycompany.any.Sleep;
 
 public class AmericanDJMegaBar extends Fixture {
     final public String name = "AmericanDJMegaBar";
-    final public int width = 9;
+    final public int width = 11;
     public int startAddress;
 
     public AmericanDJMegaBar(int _startAddress) {
         startAddress = _startAddress;
+        channels = new Channel[width];
+
         for (int i = 0; i < 9; i += 3) {
             channels[i] = new Channel(startAddress + i, channelTypes.RED);
             channels[i + 1] = new Channel(startAddress + i + 1, channelTypes.GREEN);
