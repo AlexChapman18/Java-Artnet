0775e221154dda209f4ac20f6513478f4e87d20b
lumibar <47082617+lumibar@users.noreply.github.com>
2022-02-19 18:38:50 +0000

    fixed channel num

--- src/main/java/com/mycompany/any/FixturePackage/AmericanDJMegaBar.java
+++ src/main/java/com/mycompany/any/FixturePackage/AmericanDJMegaBar.java
@@ -8,21 +8,18 @@ public class AmericanDJMegaBar extends Fixture {
     final public int width = 9;
     public int startAddress;
 
-    public AmericanDJMegaBar(int _startAddress){
+    public AmericanDJMegaBar(int _startAddress) {
         startAddress = _startAddress;
-        for (int i = 0; i < 9; i+=3){
+        for (int i = 0; i < 9; i += 3) {
             channels[i] = new Channel(startAddress + i, channelTypes.RED);
-            channels[i+1] = new Channel(startAddress + i + 1, channelTypes.GREEN);
-            channels[i+2] = new Channel(startAddress + i + 2, channelTypes.BLUE);
+            channels[i + 1] = new Channel(startAddress + i + 1, channelTypes.GREEN);
+            channels[i + 2] = new Channel(startAddress + i + 2, channelTypes.BLUE);
         }
-        channels[startAddress + 8] = new Channel(28, channelTypes.PPR);
-        channels[startAddress + 9] = new Channel(29, channelTypes.STROBE); 
+        channels[9] = new Channel(startAddress + 8, channelTypes.PPR);
+        channels[10] = new Channel(startAddress + 9, channelTypes.STROBE);
     }
 
-    public void testFixture(){
-
-        
-
+    public void testFixture() {
 
     }
 }
