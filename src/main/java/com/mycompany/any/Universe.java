890b976b7bfc49bd5d29f1bb140768edce66c87a
lumibar <47082617+lumibar@users.noreply.github.com>
2022-02-19 01:56:30 +0000

    single thread version

--- src/main/java/com/mycompany/any/Universe.java
+++ src/main/java/com/mycompany/any/Universe.java
@@ -1,26 +1,22 @@
 package com.mycompany.any;
 
 import ch.bildspur.artnet.ArtNetClient;
-import java.util.concurrent.LinkedBlockingQueue;
+//import java.util.concurrent.LinkedBlockingQueue;
 
 public class Universe {
-    LinkedBlockingQueue<byte[]> buffer = new LinkedBlockingQueue<byte[]>();
+    //LinkedBlockingQueue<byte[]> buffer = new LinkedBlockingQueue<byte[]>();
+
 
     public final String address;
     public final int subnet;
     public final int universe;
     public final ArtNetClient artnet;
 
-    protected FrameSender frameSender;
-
     public Universe(ArtNetClient _artnet, String _address, int _subnet, int _universe) {
         address = _address;
         subnet = _subnet;
         universe = _universe;
         artnet = _artnet;
-
-        frameSender = new FrameSender();
-        frameSender.start();
 
         byte[] currentFrame = new byte[512];
         for (int channel = 0; channel < currentFrame.length; channel++) {
@@ -31,7 +27,7 @@ public class Universe {
     }
 
     public void sendFrame(byte[] frame) {
-        buffer.offer(frame);
+        artnet.unicastDmx(address, subnet, universe, frame);
     }
 
 }
